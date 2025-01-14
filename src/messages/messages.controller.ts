867d2cad7dbced671c60da3733b6575cdc49b484
Hassam <haslam@technologyrivers.com>
2025-01-14 11:55:25 +0500

    initial commit

---
+++ src/messages/messages.controller.ts
@@ -0,0 +1,27 @@
+import { Body, Controller, Get, Param, Post } from '@nestjs/common';
+import { CreateMessageDTO } from './dtos/create-message.dto';
+import { MessagesService } from './messages.service';
+
+@Controller('messages')
+export class MessagesController {
+    messagesService: MessagesService
+    constructor() {
+      this.messagesService = new MessagesService()
+    }
+    @Get()
+    listMessages(){
+     const messages = this.messagesService.findAll()
+      console.log("list messages",messages);
+      return messages
+    }
+    @Post()
+    createMessage(@Body() body: CreateMessageDTO){
+      console.log("body",body)
+      return this.messagesService.create(body?.content)
+    }
+    @Get(":id")
+    getMessage(@Param("id") id:string){
+        console.log("get message by id", id)
+        return this.messagesService.findOne(id)
+    }
+}
---
+++ src/messages/messages.repository.ts
@@ -0,0 +1,27 @@
+import { readFile, writeFile } from 'fs/promises';
+
+export class MessagesRepository {
+  async findOne(id: string) {
+    // readfile and return requested id record
+    const repoMessages = await readFile('messages.json', 'utf-8');
+    const messages = JSON.parse(repoMessages);
+    return messages[id];
+  }
+  async findAll() {
+    // readfile and return all records
+    const repoMessages = await readFile('messages.json', 'utf-8');
+    const messages = JSON.parse(repoMessages);
+    return messages;
+  }
+  async create(content: string) {
+    //readfile, append new record
+    const repoMessages = await readFile('messages.json', 'utf-8');
+    const messages = JSON.parse(repoMessages);
+    const id = Math.floor(Math.random() * 999);
+    messages[id] = {
+      id,
+      content,
+    };
+    return await writeFile("messages.json",JSON.stringify(messages))
+  }
+}
